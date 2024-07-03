f61209295f362ea8cd8f784fd544dc62e2935ae2
Leo Wirasanto Laia <leowirasanto@gmail.com>
2024-07-03 12:53:27 +0800

    navigate from main menu to view roadmap selection screen

--- SwiftUIRoadMap/SwiftUIRoadMap/Core/ContentView.swift
+++ SwiftUIRoadMap/SwiftUIRoadMap/Core/ContentView.swift
@@ -9,30 +9,36 @@ import SwiftUI
 
 struct ContentView: View {
     private var roadMap = Roadmap.allCases
+    @State private var path: [Roadmap] = []
     
     var body: some View {
-        VStack {
-            Text("Choose Roadmap:")
-            
-            ForEach(roadMap, id: \.rawValue) { item in
-                Button {
-                    switch item {
-                    case .views:
-                        break
-                    default:
-                        break
+        NavigationStack(path: $path) {
+            VStack {
+                Text("Choose Roadmap:")
+                
+                ForEach(roadMap, id: \.rawValue) { item in
+                    Button {
+                        path = [item]
+                    } label: {
+                        Text(item.description)
+                            .padding(8)
+                            .foregroundStyle(.white)
+                            .background(.orange)
+                            .fontWeight(.bold)
                     }
-                } label: {
-                    Text(item.description)
-                        .padding(8)
-                        .foregroundStyle(.white)
-                        .background(.orange)
-                        .fontWeight(.bold)
+                    .clipShape(RoundedRectangle(cornerRadius: 10))
                 }
-                .clipShape(RoundedRectangle(cornerRadius: 10))
+            }
+            .padding()
+            .navigationDestination(for: Roadmap.self) { selected in
+                switch selected {
+                case .views:
+                    ViewsRoadmapSelection()
+                default:
+                    Text("Coming soon")
+                }
             }
         }
-        .padding()
     }
 }
 
