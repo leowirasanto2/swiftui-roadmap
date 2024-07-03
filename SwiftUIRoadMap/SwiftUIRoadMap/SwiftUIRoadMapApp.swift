c3a756c19d0fcfb077e46032839073b608cecfd8
Leo Wirasanto Laia <leowirasanto@gmail.com>
2024-07-03 12:04:19 +0800

    add xcode project

---
+++ SwiftUIRoadMap/SwiftUIRoadMap/SwiftUIRoadMapApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIRoadMapApp.swift
+//  SwiftUIRoadMap
+//
+//  Created by Leo Wirasanto Laia on 03/07/24.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIRoadMapApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
