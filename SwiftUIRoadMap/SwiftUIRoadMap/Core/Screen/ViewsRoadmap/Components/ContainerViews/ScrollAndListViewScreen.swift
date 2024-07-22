f96418ee519ff04b0f569255be4accaecc08d54c
Leo Wirasanto Laia <leowirasanto@gmail.com>
2024-07-22 15:11:22 +0800

    create list view with dynamic list style

---
+++ SwiftUIRoadMap/SwiftUIRoadMap/Core/Screen/ViewsRoadmap/Components/ContainerViews/ScrollAndListViewScreen.swift
@@ -0,0 +1,96 @@
+//
+//  ScrollAndListViewScreen.swift
+//  SwiftUIRoadMap
+//
+//  Created by Leo Wirasanto Laia on 22/07/24.
+//
+
+import SwiftUI
+
+struct ScrollAndListViewScreen: View {
+    private var styles: [String] = ListViewStyle.allCases.map { $0.rawValue }
+    @State private var selectedStyle = ListViewStyle.grouped.rawValue
+    @State private var dataList: [ListData] = [
+        ListData(
+            title: "Sports",
+            items: [
+                ListDataItem(text: "🏈"),
+                ListDataItem(text: "🏀"),
+                ListDataItem(text: "🥎"),
+                ListDataItem(text: "⚽️")
+            ]
+        ),
+        ListData(
+            title: "Art",
+            items: [
+                ListDataItem(text: "🎨"),
+                ListDataItem(text: "🎶"),
+                ListDataItem(text: "🎸"),
+                ListDataItem(text: "🥁")
+            ]
+        )
+    ]
+    
+    var body: some View {
+        VStack(alignment: .leading, spacing: 16) {
+            Picker("Style", selection: $selectedStyle) {
+                ForEach(styles, id: \.self) { item in
+                    Text(item)
+                }
+            }
+            
+            switch ListViewStyle(rawValue: selectedStyle) {
+            case .sidebar:
+                contentView
+                    .listStyle(SidebarListStyle())
+            case .inset:
+                contentView
+                    .listStyle(InsetListStyle())
+            case .insetGrouped:
+                contentView
+                    .listStyle(InsetGroupedListStyle())
+            case .plain:
+                contentView
+                    .listStyle(PlainListStyle())
+            default:
+                contentView
+                    .listStyle(GroupedListStyle())
+            }
+        }
+    }
+    
+    var contentView: some View {
+        return List {
+            ForEach(dataList, id: \.id) { section in
+                Section(section.title) {
+                    ForEach(section.items, id: \.id) { section in
+                        Text(section.text)
+                    }
+                }
+            }
+        }
+    }
+}
+
+#Preview {
+    ScrollAndListViewScreen()
+}
+
+struct ListData {
+    var id = UUID().uuidString
+    var title: String
+    var items: [ListDataItem] = []
+}
+
+struct ListDataItem {
+    var id = UUID().uuidString
+    var text: String
+}
+
+enum ListViewStyle: String, CaseIterable {
+    case sidebar = "Sidebar"
+    case inset = "Inset"
+    case insetGrouped = "Inset Grouped"
+    case plain = "Plain"
+    case grouped = "Grouped"
+}
